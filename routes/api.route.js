cdca70185436b55fd07a198d58eb39ff9d42ae5a
Milad Akbari <miladakbari.dev@gmail.com>
2022-05-21 10:21:34 +0430

    created patch request for products

--- routes/api.route.js
+++ routes/api.route.js
@@ -68,7 +68,22 @@ router.delete('/products/:id', async (req, res, next) => {
 })
 
 router.patch('/products/:id', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' })
+  try {
+    const { id } = req.params
+    const product = await prisma.product.update({
+      where: {
+        id: Number(id)
+      },
+      data: req.body,
+      include: {
+        Category: true
+      }
+    })
+
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
